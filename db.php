47e19443ee6c5d63509e8cf000986fb937d6c99e
manideep-udumula <79364298+manideep-udumula@users.noreply.github.com>
2021-11-29 21:55:38 +0530

    Update db.php
    connection change

--- db.php
+++ db.php
@@ -1,9 +1,22 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost","root","","spandana");
+ $servername = "remotemysql.com";
+$username = "WK1eggxcgK";
+$password = "Q7gkaeCv2k";
+$dbname = "WK1eggxcgK";
+
+// Create connection
+$con = new mysqli($servername,
+    $username, $password, $dbname);
+
+
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 ?>
+
+
+
+
