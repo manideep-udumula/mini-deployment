887becd1cdcdce6253df911b8a10ff3028357afc
manideep-udumula <79364298+manideep-udumula@users.noreply.github.com>
2021-12-08 11:11:25 +0530

    Update expert_dashboard.php

--- expert_dashboard.php
+++ expert_dashboard.php
@@ -30,7 +30,7 @@ while($p=mysqli_fetch_assoc($r))
 
 <form method="post">
     <label for="answer"value="<?php echo $p['query']; ?>" ></label>
-  <!--- <input type="text" "background:none ; outline:none;" name="query" value="<?php echo $p['query']; ?>" /> --->
+
     <input type="text" name="answer"   placeholder="Answer here"/>
     <button  name="subm">Submit</button>   <br> <br>
 </form>
