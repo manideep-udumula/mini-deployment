7772687947b476a37a6fc3782624952624d88e17
manideep-udumula <79364298+manideep-udumula@users.noreply.github.com>
2021-12-08 22:46:43 +0530

    Update inbox.php

--- inbox.php
+++ inbox.php
@@ -1,26 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css" />
-</head>
-<body bgcolor="#0f8a9d">
-    <center>
 
-<ul>
-  <li><a href="dashboard.php">Home</a></li>
-
-  <li><a href="contact.html">Contact</a></li>
-  <li style="float:right"><a class="active" href="logout.php">Logout</a></li>
-</ul>
 <?php
 
 require('db.php');  
 
 $user=$_SESSION['username'];
 
-$q="select query,answer from queries where username='$user'";
+$q="select query,answer from queries where username= ['$user']";
 
 $re=mysqli_query($con,$q);
 while($p=mysqli_fetch_assoc($re))
@@ -44,6 +29,3 @@ while($p=mysqli_fetch_assoc($re))
     }
 }
 ?>
-</center>
-</body>
-</html>
